8f4f46c520788a12fe5d5938f6412544dc617265
Aleksander Szut <szut.aleksander@gmail.com>
2020-04-26 00:08:10 +0200

    Szkielet strony

---
+++ js/app.js
@@ -0,0 +1,144 @@
+
+/*Własne funkcje porównywania znaków*/
+
+let sortDirection = false;
+let tableHand;
+let dataList;
+let tbody;
+
+function sortDateCol(sort, colID) {
+    dataList.rows = dataList.rows.sort((p1,p2) =>{
+        return sort ? new Date(p1[colID]) - new Date(p2[colID]) : new Date(p2[colID]) - new Date(p1[colID]);
+    });
+    console.log("Date");
+}
+function sortNumberCol(sort, colID) {
+    dataList.rows = dataList.rows.sort((p1,p2) =>{
+        return sort ? p1[colID] - p2[colID] : p2[colID] - p1[colID]  ;
+    });
+}
+function sortStringCol(sort, colID) {
+    dataList.rows = dataList.rows.sort((p1,p2) =>{
+        return sort ? p1[colID].localeCompare(p2[colID]) : p2[colID].localeCompare( p1[colID])  ;
+    });
+}
+/*Funkcja sortującja po kolumnie tabeli*/
+function sortColumn(colID, specialType)
+{
+    sortDirection = !sortDirection;
+    const dataType = typeof dataList.rows[0][colID];
+
+    let sortedCol = document.querySelectorAll('.sortedColumn');
+    for(let sortedItem of sortedCol)
+        sortedItem.classList.remove('sortedColumn');
+    let thisCol = document.getElementById('tableColHeader'+colID);
+    thisCol.classList.add('sortedColumn');
+    if(sortDirection)
+        thisCol.classList.add("sortDesc");
+    else
+        thisCol.classList.remove("sortDesc");
+
+    if(typeof specialType != 'undefined')
+    {
+        switch (specialType) {
+            case 'date':
+                sortDateCol(sortDirection, colID);
+                break;
+            case 'nosort':
+                break;
+            default:
+                sortNumberCol(sortDirection, colID);
+        }
+
+    }
+    else
+    {
+        switch (dataType) {
+            case 'number':
+                sortNumberCol(sortDirection, colID);
+                break;
+            case 'string':
+                sortStringCol(sortDirection, colID);
+                break;
+        }
+    }
+
+    setBodyRow();
+}
+
+function setBodyRow() {
+    let innerHTML = '';
+
+    for(let row of dataList.rows)
+    {
+        innerHTML += '<tr class="datatable datatable__row"> ';
+        let i=0;
+        for(let data of row)
+        {
+            let specialClass = '';
+            if(dataList.col[i].specialClass != null)
+            {
+                specialClass = dataList.col[i].specialClass;
+            }
+            innerHTML += `<td class="${specialClass}">${data}</td>`;
+
+            i++
+        }
+        innerHTML += '</tr>';
+    }
+    tbody.innerHTML = innerHTML;
+}
+
+function loadTableBody() {
+    tbody = document.createElement("tbody");
+    tbody.classList.add("datatable");
+    tbody.classList.add("datatable__body");
+    tableHand.appendChild(tbody);
+
+    setBodyRow();
+}
+
+function initDataTables(elementHandler, tableObject) {
+
+    tableHand = elementHandler;
+    dataList = tableObject;
+
+
+    /*Load head*/
+    if (typeof tableObject.head !== "undefined")
+    {
+
+        let thead = document.createElement("thead");
+        let tr = document.createElement("tr");
+        thead.classList.add("datatable");
+        thead.classList.add("datatable__header");
+        thead.appendChild(tr);
+
+        let tdElements = '';
+        let i = 0;
+        for (let tdElement of tableObject.head) {
+            let specialType = '';
+            if(tableObject.col[i].type != null)
+            {
+                console.log(tableObject.col[i].type);
+                specialType = `, '`+tableObject.col[i].type+`'`;
+            }
+            tdElements += `<th id="tableColHeader${i}" class="" onclick="sortColumn(${i}${specialType})">${tdElement}</th>`;
+
+            i++;
+        }
+        elementHandler.appendChild(thead).innerHTML = `<tr>${tdElements}</tr>`;
+    }
+    else
+        console.error('Objekt danych nie posiada nagłówka "head"');
+
+    /*Load body*/
+
+    if (typeof tableObject.rows !== "undefined")
+    {
+        loadTableBody(dataList);
+    }
+    else
+        console.error('Objekt danych nie posiada elementu "rows"');
+
+}
